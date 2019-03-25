8eb50b149b21c532439716e0cb5ca65f81053847
k-yusuke-0723 <kamiyashikiyusuke@kamiyashikiyuusukenoMacBook-puro.local>
2019-03-25 18:55:49 +0900

    fix:関連ファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-root 't#index'
-
   devise_for :users
-  get 't' => 't#index'
-  get 'usercheck' => 'test_views#usercheck'
-  get 'signup' => 'test_views#signup'
-  get 'login' => 'test_views#login'
-  get 'top' => 'test_views#top'
-  get 'creditcard' => 'test_views#creditcard'
-
+  resources :test_views do
+    get :creditcard
+  end
 end
