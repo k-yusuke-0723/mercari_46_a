2a04f93e9adff040ca0ab519176e9918631b7f13
autumnleaves111 <aprncc972@gmail.com>
2019-03-26 20:21:00 +0900

    指摘点の改善
    ・ルーティングをresourcesを使用したものに変更
    Memberメソッドを用いてusersコントローラにlogoutアクションを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :credit_card, only: [:new]
-  get 'logout' => 'test_views#logout'
+  resources :users do
+    get :logout, on: :member
+  end
 end
