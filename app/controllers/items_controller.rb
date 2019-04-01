e15b390e221f8674d74c8bc8ffd40770be6fd6b4
k-yusuke-0723 <kamiyashikiyusuke@kamiyashikiyuusukenoMacBook-puro.local>
2019-04-01 11:11:21 +0900

    add:商品出品機能の実装
    商品出品機能の実装。
    ・カテゴリー機能未実装
    ・現時点画像投稿1枚のみ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+    @item.build_image
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "mypages/show"
+    else
+      render "items/new"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :description,
+      :state,
+      :delivery_fee_payer,
+      :shipping_method,
+      :shipping_source_area,
+      :estimated_shipping_date,
+      :price,
+      image_attributes: [:image1]).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :user
+  has_one :image, dependent: :destroy
+  accepts_nested_attributes_for :image
+  validates :name,                    presence: true, length: { maximum: 40 }
+  validates :description,             presence: true, length: { maximum: 1000 }
+  validates :state,                   presence: true
+  validates :delivery_fee_payer,      presence: true
+  validates :shipping_method,         presence: true
+  validates :shipping_source_area,    presence: true
+  validates :estimated_shipping_date, presence: true
+  validates :price,                   presence: true
 end
