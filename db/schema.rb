f6a63350553374bc6bbf3ba18fa526ba37d0a971
Smith11764 <fuji.feather10@gmail.com>
2019-04-04 15:38:38 +0900

    userテーブルmigrationファイルの統一

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_125731) do
+ActiveRecord::Schema.define(version: 2019_04_01_075811) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id"
@@ -64,11 +64,6 @@ ActiveRecord::Schema.define(version: 2019_04_02_125731) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -82,12 +77,18 @@ ActiveRecord::Schema.define(version: 2019_04_02_125731) do
     t.integer "block"
     t.integer "building"
     t.string "phone_number"
+    t.string "string"
     t.integer "point"
     t.date "point_exp_date"
     t.text "biography"
     t.string "user_image"
     t.string "uid"
     t.string "provider"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
